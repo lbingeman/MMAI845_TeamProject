d9d9d3694dd29c758c7947302ba410bbd46e76b4
lbingeman <l.bingeman@icloud.com>
2025-03-16 12:44:33 -0400

    updae model

--- FarmDeliverySystem.py
+++ FarmDeliverySystem.py
@@ -8,7 +8,7 @@ import sys
 alpha = 0.1
 gamma = 0.9
 epsilon = 0.1
-num_episodes = 10000000
+num_episodes = 10000
 num_steps = 1000
 
 class QTable:
@@ -18,10 +18,10 @@ class QTable:
 
     def get_q_value(self, state, action):
         if state not in self.q_table:
-            self._create_q_values()
+            self._create_q_values(state)
         return self.q_table[state][action]
     
-    def _create_q_values(self):
+    def _create_q_values(self, state):
         self.q_table[state] = []
         for action in self.actions:
             self.q_table[state].append(0.0)
@@ -29,7 +29,7 @@ class QTable:
     def get_q_values(self, state):
         # Create q-table if it does not exist
         if state not in self.q_table:
-            self._create_q_values()
+            self._create_q_values(state)
         return self.q_table[state]
             
     def set_q_value(self, state, action, value):
@@ -39,7 +39,11 @@ if __name__=='__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "q_learning":
         env = FarmEnv(render_mode='not_human')
         
-        q_table = QTable(actions=range(env.action_space.n))
+        if len(sys.argv) > 2 and sys.argv[1] == "continue":
+            with open("current_q_table.pkl", "rb") as f:
+                q_table = pickle.load(f)
+        else:
+            q_table = QTable(actions=range(env.action_space.n))
         
         for episode in range(num_episodes):
             (state, info) = env.reset()
@@ -84,13 +88,30 @@ if __name__=='__main__':
         # Save the object to a file
         with open("current_q_table.pkl", "wb") as f:
             pickle.dump(q_table, f)
+    if len(sys.argv) > 1 and sys.argv[1] == "prev_trained_table":
+        # Load the object from the file
+        with open("current_q_table.pkl", "rb") as f:
+            q_table = pickle.load(f)
+        env = FarmEnv(render_mode='human')
+        (next_state, info) = env.reset()
+        for i in range(num_steps):
+            action_mask = info["action_mask"]
+            q_values = q_table.get_q_values(next_state)
+            action = 0
+            top_value = np.iinfo(np.int32).min
+            for potential_action, value in enumerate(q_values):
+                if value >= top_value and action_mask[potential_action] != 0:
+                    action = potential_action
+                    top_value = value
+            (next_state, reward, terminated, truncated, info) = env.step(action)
+            if terminated:
+                break
     else:
         env = FarmEnv(render_mode='human')
         (next_state, info) = env.reset()
         
         total_reward = 0
         while True:
-            env.render()
             action_mask = info["action_mask"]
             
             # choose an action from valid actions
